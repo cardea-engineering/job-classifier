aa6c97b9b97adcd3bfc48d295cfd8497f0088450
Guandong Kou <gdkou90@gmail.com>
2020-10-26 06:47:32 -0400

    refactor: separated prediction module from server

--- server.py
+++ server.py
@@ -1,22 +1,8 @@
-import numpy as np
 from flask import Flask, request, jsonify
 from utils.utils import *
-from utils.match_rules import get_match
-
-
-job_category_model = get_serialized('job_type_category_name_model.pkl')
-job_type_model = get_serialized('job_type_name_model.pkl')
-tfidfVectorizer = get_serialized('tfIdfVectorizer.pkl')
 
 
 app = Flask(__name__)
-
-
-def wrap_result(names, probs):
-    return sorted([
-        {'name': names[i], 'probability': float('%.3f' % p)}
-        for i, p in enumerate(probs)], key=lambda x: -x['probability']
-    )
 
 
 @app.route('/predict', methods=['POST'])
@@ -25,24 +11,10 @@ def results():
     data = request.form
     job_title = data['title']
     job_desc = data['desc']
-    text_input = parse_raw_html(job_title + job_desc)
 
-    Xts = tfidfVectorizer.transform(np.array([text_input])).toarray()
+    result = predict(job_title, job_desc)
 
-    type_matched = get_match(job_title, job_desc)
-    if type_matched:
-        result_type = {type_matched: 1}
-    else:
-        prob_types = job_type_model.predict_proba(Xts).flatten()
-        result_type = wrap_result(job_types, prob_types)
-
-    prob_category = job_category_model.predict_proba(Xts).flatten()
-    result_category = wrap_result(job_categories, prob_category)
-
-    return jsonify({
-        'job_category': result_category,
-        'job_type': result_type
-    })
+    return jsonify(result)
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -2,11 +2,21 @@ import re
 import html
 import unicodedata
 import pickle
+import numpy as np
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from utils.match_rules import get_match
 
+
+def get_model(file_name):
+    return pickle.load(open('models/' + file_name, 'rb'))
+
+
+job_category_model = get_model('job_type_category_name_model.pkl')
+job_type_model = get_model('job_type_name_model.pkl')
+tfidfVectorizer = get_model('tfIdfVectorizer.pkl')
 
 job_categories = ['Business Operations', 'Data & Analytics',
                   'Finance, Legal & Compliance', 'Product & Design',
@@ -37,5 +47,28 @@ def parse_raw_html(raw_html):
     return ""
 
 
-def get_serialized(file_name):
-    return pickle.load(open('static/' + file_name, 'rb'))
+def wrap_result(names, probs):
+    return sorted([
+        {'name': names[i], 'probability': float('%.3f' % p)}
+        for i, p in enumerate(probs)], key=lambda x: -x['probability']
+    )
+
+
+def predict(job_title, job_desc):
+    text_input = parse_raw_html(job_title + job_desc)
+    Xts = tfidfVectorizer.transform(np.array([text_input])).toarray()
+    
+    type_matched = get_match(job_title, job_desc)
+    if type_matched:
+        result_type = {type_matched: 1}
+    else:
+        prob_types = job_type_model.predict_proba(Xts).flatten()
+        result_type = wrap_result(job_types, prob_types)
+
+    prob_category = job_category_model.predict_proba(Xts).flatten()
+    result_category = wrap_result(job_categories, prob_category)
+
+    return {
+        'job_category': result_category,
+        'job_type': result_type
+    }
