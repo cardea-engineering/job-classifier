a0e6fe34f16fb8cd981ed400f890c9de36781f5f
Guandong Kou <gdkou90@gmail.com>
2020-11-10 11:33:11 -0500

    chore: fix invalid import script

--- train/preprocessing.py
+++ train/preprocessing.py
@@ -18,5 +18,36 @@ from utils.match_rules import match_job_type_rules
 # serialize models
 import pickle
 
-# import feature engineering functions from utilities
-from utils.utils import parse_raw_html
+
+# prepare data
+def get_X_y(df):
+
+    type_counter = Counter(df['job_type_name'].tolist())
+    category_counter = Counter(df['job_type_category_name'].tolist())
+    top_10_types = {name for name, _ in type_counter.most_common(10)}
+    top_5_categories = {name for name, _ in category_counter.most_common(5)}
+    pred_field_name = 'job_type_name'
+    field_map = {
+        'job_type_category_name': top_5_categories,
+        'job_type_name': top_10_types
+    }
+    if pred_field_name in field_map:
+        df = df[df[pred_field_name].map(lambda x: x in field_map[pred_field_name])]
+
+    df['desc_tokenized'] = df.apply(lambda row: parse_raw_html(str(row['description']) + row['title']), axis=1)
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    tfIdfVectorizer = TfidfVectorizer(
+        analyzer='word', 
+        sublinear_tf=True,
+        stop_words=STOP_WORDS,
+        strip_accents='unicode',
+        token_pattern=r'\w{2,}',
+        ngram_range=(1,1),
+        max_features=1000,
+        use_idf=True
+    )
+    tfIdf = tfIdfVectorizer.fit_transform(list(df['desc_tokenized']))
+    X = tfIdf.toarray() # convert to dense array
+    job_types, y = np.unique(df[pred_field_name], return_inverse=True)
+    return X, y, job_types
