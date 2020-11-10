b8e587ca4e28697b66183d9dc2fd8f2e3d46f16b
Guandong Kou <gdkou90@gmail.com>
2020-11-10 14:50:02 -0500

    refactor: added metrics for manual rules

--- train/preprocessing.py
+++ train/preprocessing.py
@@ -22,6 +22,7 @@ import pickle
 # global variables
 RAND_SEED = 42
 TRAIN_FIELD = 'job_type_name'
+TRAIN_FRACTION = 0.67
 
 
 def get_dataFrame():
@@ -58,9 +59,8 @@ def get_X_y(df):
     return X, y, job_types
 
 
-def get_train_test(X, y, train_size = 0.67):
-    n_samples = len(X)
-    n_train = int(n_samples * train_size)
+def get_train_test(X, y, train_size = TRAIN_FRACTION):
+    n_train = int(len(X) * train_size)
     Xtr, Xts = X[:n_train, :], X[n_train:, :]
     ytr, yts = y[:n_train], y[n_train:]
     return Xtr, Xts, ytr, yts
@@ -80,4 +80,21 @@ def get_result(model, Xts, yts, job_types):
     yhat = model.predict(Xts)
     report = classification_report(yhat, yts, labels=[i for i in range(10)], target_names=job_types, digits=3)
     print('prediction metrics without manual rules')
+    print(report)
+
+
+def get_metrics_with_rules(df, Xts, yts, yhat, job_types):
+    c = Counter()
+    job_type_2_class_id = {job_type: i for i, job_type in enumerate(job_types)}
+    n_train = int(len(df) * TRAIN_FRACTION)
+
+    for i, v in enumerate(Xts):
+        title = df.iloc[n_train+i]['title']
+        matched_type = match_job_type_rules(title.lower())
+        if matched_type:
+            if yhat[i] != job_type_2_class_id[matched_type]:
+                c[matched_type] += 1
+                yhat[i] = job_type_2_class_id[matched_type]
+    report = classification_report(yhat, yts, labels=[i for i in range(10)], target_names=job_types, digits=3)
+    print('prediction metrics after applying manual rules')
     print(report)
